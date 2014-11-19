590d2db44b3bb87b6c3c11dda149e574c760cf10
Kevin Liang <keviekev23@gmail.com>
2014-11-18 17:12:21 -0800

    updated API

--- libraries/WWRobotAPI.framework/Versions/A/Headers/WWSensorDistance.h
+++ libraries/WWRobotAPI.framework/Versions/A/Headers/WWSensorDistance.h
@@ -11,14 +11,16 @@
 /**
  *  The `WWSensorDistance` object provides estimated distance data from `WWRobot` (read-only).  
  *
- *  Distance data is calculated based on the reflectance of the IR sensors on the robot, so it is heavily based on 
- *  the environment visibility (e.g. dimmed room) and object reflectance (e.g. white vs. black).
+ *  Reflectance data is returned as the raw data to calculate distance from.  Reflectance value is based 
+ *  on the light reading of the IR sensors on the robot, so calculating the actual distance from this data 
+ *  will be heavily based on the environment visibility (e.g. dark vs. incandescent light) and object's 
+ *  reflectivity (e.g. white vs. black).
  */
 @interface WWSensorDistance : WWSensor
 
 /**
- *  Returns calculated distance (in CM), using proprietary algorithm.
+ *  Returns the averaged light reflectance received by the sensor.
  */
-@property (nonatomic, readonly) double distance; 
+@property (nonatomic, readonly) double reflectance;
 
 @end
---
+++ playground/ControlSoundViewController.h
@@ -0,0 +1,13 @@
+//
+//  ControlSoundViewController.h
+//  playground
+//
+//  Created by Kevin Liang on 11/18/14.
+//  Copyright (c) 2014 Wonder Workshop. All rights reserved.
+//
+
+#import "RobotControlViewController.h"
+
+@interface ControlSoundViewController : RobotControlViewController
+
+@end
