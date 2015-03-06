e12aaa0dd7c58f0bb747c7ae42dcd36b58c00fcc
Kevin Liang <keviekev23@gmail.com>
2015-03-06 12:34:53 -0800

    use degrees for head

--- Android/Playground/app/src/main/java/fragments/HeadControlFragment.java
+++ Android/Playground/app/src/main/java/fragments/HeadControlFragment.java
@@ -27,11 +27,11 @@ public class HeadControlFragment extends BaseFragment {
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View rootView = inflater.inflate(R.layout.fragment_head_control, container, false);
 
-    rootView.findViewById(R.id.button_head_top).setOnClickListener(createOnClickListener(Math.toRadians(90), 0));
-    rootView.findViewById(R.id.button_head_left).setOnClickListener(createOnClickListener(0, Math.toRadians(-15)));
+    rootView.findViewById(R.id.button_head_top).setOnClickListener(createOnClickListener(-15, 0));
+    rootView.findViewById(R.id.button_head_left).setOnClickListener(createOnClickListener(0, -90));
     rootView.findViewById(R.id.button_head_middle).setOnClickListener(createOnClickListener(0, 0));
-    rootView.findViewById(R.id.button_head_right).setOnClickListener(createOnClickListener(0, Math.toRadians(15)));
-    rootView.findViewById(R.id.button_head_bottom).setOnClickListener(createOnClickListener(Math.toRadians(-90), 0));
+    rootView.findViewById(R.id.button_head_right).setOnClickListener(createOnClickListener(0, 90));
+    rootView.findViewById(R.id.button_head_bottom).setOnClickListener(createOnClickListener(5, 0));
 
     return rootView;
   }
